151913f2e250da8bf6e5c2a64a11d689154cc0c6
Sumaiya-Meem <sumaiyameem.cse6.bu@gmail.com>
2024-03-29 13:38:35 +0600

    connect mongodb

--- index.js
+++ index.js
@@ -8,6 +8,29 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ojnnavp.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+  
+
+    
+   console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
